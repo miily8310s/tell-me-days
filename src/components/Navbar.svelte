0f1caa596b5c01221771e0726827b95a27a40a1f
miily8310s <rillakerika@gmail.com>
2021-04-11 18:43:26 +0900

    add navbar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,8 @@
+<nav class="w-full bg-green-500 mb-3">
+  <div class="flex px-6 py-3 justify-center items-center">
+    <div class="flex flex-col  text-gray-50">
+      <h1 class="text-5xl font-semibold">Tell you days</h1>
+      <span class="text-2xl py-2">- あなたの出社日教えます -</span>
+    </div>
+  </div>
+</nav>
