59dff50c4857b875e366caa235e832fcdbb0c608
miily8310s <rillakerika@gmail.com>
2021-04-11 19:44:31 +0900

    add date checkbox

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import Tailwindcss from './Tailwindcss.svelte';
 	import Navbar from "./components/Navbar.svelte";
-
+	import Form from "./components/Form.svelte";
 </script>
 
 <Tailwindcss />
 <Navbar />
+<Form />
---
+++ src/components/Form.svelte
@@ -0,0 +1,44 @@
+<div class="flex flex-col justify-center items-center my-6">
+  <h2 class="font-semibold text-xl mb-3">年月と曜日を選択することで該当する日付を教えてくれるアプリです。</h2>
+  <div class="shadow-md p-6 w-full md:w-2/3 mb-6">
+    <div class="flex flex-row items-center text-2xl leading-4 mb-6">
+      <label for="" class="mr-10 font-semibold">曜日</label>
+      <div class="flex flex-row">
+        <div class="mr-3">
+          <input type="checkbox">
+          <span>月</span>
+        </div>
+        <div class="mr-3">
+          <input type="checkbox">
+          <span>火</span>
+        </div>
+        <div class="mr-3">
+          <input type="checkbox">
+          <span>水</span>
+        </div>
+        <div class="mr-3">
+          <input type="checkbox">
+          <span>木</span>
+        </div>
+        <div class="mr-3">
+          <input type="checkbox">
+          <span>金</span>
+        </div>
+        <div class="mr-3">
+          <input type="checkbox">
+          <span>土</span>
+        </div>
+        <div class="mr-3">
+          <input type="checkbox">
+          <span>日</span>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-row items-center text-2xl leading-4 mb-6">
+      <label for="" class="mr-10 font-semibold">年月</label>
+    </div>
+  </div>
+  <button class="text-xl font-semibold leading-4 p-3 text-white bg-blue-500 rounded">日付を教えてもらう</button>
+  <div class="shadow-md p-6 w-full md:w-2/3 mt-3">
+  </div>
+</div>
